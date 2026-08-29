b3982653ba599a5c96530520c87f0c755cf09e01
FRANK3308 <frankylapagol@gmail.com>
2026-08-29 18:08:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -98,10 +98,10 @@ button.addEventListener("click", async () => {
     document.getElementById("streaming-details-80s").style.display = "none";
     document.querySelector(".player80s").style.display = "block";
 
-    playCurrentVideo();
-    showControls();
-});
-
+    if (!document.fullscreenElement) {
+        await container.requestFullscreen().catch(err => {
+            console.log("Error al activar pantalla completa:", err);
+        });
     }
 
     playCurrentVideo();
